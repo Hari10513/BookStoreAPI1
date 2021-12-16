07efc30893483da0896e30feac86483e011705ac
Bharath <Bharath@LAPTOP-IS8K54U6>
2021-12-16 16:27:39 +0530

    Add project files.

---
+++ BookStoreAPI1/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using BookStoreAPI1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public List<Book> books = new List<Book>()
+        {
+            new Book { Id = 1, Title = "The Girl on the train", Author = "Hawkins, Paula", PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 2, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 3, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 4, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 5, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 6, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 7, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 8, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 9, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"},
+            new Book { Id = 10, Title = "The Girl on the train", Author = "Hawkins, Paula",PublicationYear = 2017, CallNumber = "7904932295"}
+        };
+
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Book>> GetAllBooks()
+        {
+            return books;
+        }
+
+        [HttpGet("{id}")]
+
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if(book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+
+
+
+    }
+}
---
+++ BookStoreAPI1/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3", "Value4", "Value5" };
+        }
+
+        [HttpGet("{id}")]
+
+        public string Get(int id)
+        {
+            return "The value is " + id;
+        }
+
+    }
+}
